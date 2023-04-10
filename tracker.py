66176fdadb34f069368ef85dc43a0a0e5f86f1d1
SPAMallday <pjw090@gmail.com>
2023-04-11 01:37:15 +0900

    [REMOVE] TODO

--- tracker.py
+++ tracker.py
@@ -12,13 +12,9 @@ fourcc = cv.VideoWriter_fourcc('m','p','4','v')
 cv.VideoWriter(저장 파일명, fourcc 코덱, 프레임 수, (가로 크기, 세로 크기))
 
 note :
-아마 변환과정에서 30프레임으로 고정시켜야 할 듯 + 오디오랑 싱크를 맞추기 위해 프레임 신경 쓰기
+아마 변환 과정에서 30프레임으로 고정시켜야 할 듯 + 오디오랑 싱크를 맞추기 위해 프레임 신경 쓰기
 '''
 
-#TODO 2. 윈도우 속성으로 보는 너비와 높이랑 다른 이유?
-#TODO 3. 역으로 뒤집어서 1080x1920 사이즈로 저장하면 카톡 비디오 압축 시 왜 다르게 인식하는지?
-#TODO 5. H.265로 하면 HEVC 처리가 가능하다는데,,
-# 아이폰에서도 고효율성 (HEVC) / 높은 호환성 (JPEG/H.264) 두가지로 녹화된다
 
 width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
 height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
